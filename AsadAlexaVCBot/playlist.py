c38b93c98ea74d2672d7ab1c54e1d96b82f8745a
Emilia <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-19 01:00:37 +0700

    Update playlist.py

--- AsadAlexaVCBot/playlist.py
+++ AsadAlexaVCBot/playlist.py
@@ -14,19 +14,19 @@ async def playlist(client, m: Message):
             chat_queue = get_queue(chat_id)
             if len(chat_queue) == 1:
                 await m.reply(
-                    f"**🎧 ɴᴏᴡ ᴘʟᴀʏɪɴɢ:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`"
+                    f"**🎧 Sekarang Diputar:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`"
                 )
             else:
-                QUE = f"**🎧 ɴᴏᴡ ᴘʟᴀʏɪɴɢ:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
+                QUE = f"**🎧 Sekarang Diputar:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
                 keyboard = InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton(
-                                "❤️ ᴏᴡɴᴇʀ ❤️", url=f"https://t.me/Dr_Asad_Ali"
+                                "✨ Pemilik ✨", url=f"https://t.me/tth_kiya98"
                             ),
                             InlineKeyboardButton(
-                                "👨‍‍👧‍👦 ɢʀᴏᴜᴘ 👨‍👧‍👦",
-                                url=f"https://t.me/Shayri_Music_Lovers",
+                                "✨ Grup ✨",
+                                url=f"https://t.me/CatatanAzDay",
                             ),
                         ]
                     ]
@@ -39,4 +39,4 @@ async def playlist(client, m: Message):
                     QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
                 await m.reply(QUE, disable_web_page_preview=True)
         else:
-            await m.reply("😜 **ɪᴛᴛᴜ sᴇʏ ᴘᴀɢᴀʟ sᴏɴɢ ᴄʜᴀʟᴀ ʟᴇʏ ᴘᴇʜʟʏ**")
+            await m.reply("😁 **Nggak Tahu harus  **")
